6ad6b0794539aa016969e64b0904a5e4fca50835
Skylar Broad <skylarb@remitly.com>
2020-10-21 15:07:35 -0700

    add stuff

--- goplay.go
+++ goplay.go
@@ -6,10 +6,18 @@ import "fmt"
 type Player struct {
 	Name string
 	Score int
+	Won bool
 }
 
 func (p *Player) Play() {
-	fmt.Printf("Player %s is playing with a score of %d\n", p.Name, p.Score)
+	if p.Score > 10 {
+		p.Won = true
+	}
+	if p.Won {
+		fmt.Printf("Player %s won with a score of %d\n", p.Name, p.Score)
+	} else {
+		fmt.Printf("Player %s is playing with a score of %d\n", p.Name, p.Score)
+	}
 }
 
 // Play it's like work but more fun
