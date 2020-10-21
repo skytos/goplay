6107635c400c657ded1529093b4643d40f062626
Skylar Broad <skylarb@remitly.com>
2020-10-21 14:39:34 -0700

    add a type

--- goplay.go
+++ goplay.go
@@ -2,6 +2,16 @@ package goplay
 
 import "fmt"
 
+// Player's can Play
+type Player struct {
+	Name string
+	Score int
+}
+
+func (p *Player) Play() {
+	fmt.Printf("Player %s is playing with a score of %d\n", p.Name, p.Score)
+}
+
 // Play it's like work but more fun
 func Play() {
 	fmt.Println("Play!")
